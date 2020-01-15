ab00677d23f4cb710470cc7855a6eefe8a89de7c
oferMoses <10ofer0@gmail.com>
2020-01-15 19:25:03 +0200

    21:25

--- src/main/java/bgu/spl/net/impl/stomp/DISCONNECT.java
+++ src/main/java/bgu/spl/net/impl/stomp/DISCONNECT.java
@@ -17,10 +17,10 @@ public class DISCONNECT implements Command {
 
     @Override
     public String execute() {
-        if(protocol.getActiveUsername().compareTo("default")==0){
-           Error e = new Error("","user is not logged in","","tried to logout but no user is logged in " ,protocol);
-           return e.execute();
-       }
+//        if(protocol.getActiveUsername().compareTo("default")==0){ //TODO:ofer: think this error is not needed
+//           Error e = new Error("","user is not logged in","","tried to logout but no user is logged in " ,protocol);
+//           return e.execute();
+//       }
         ConnectionsImpl connections = protocol.getConnections();
         int connectId = protocol.getConnectionId();
         for(Pair<Integer,String> topic :protocol.getMyTopics()){ //delete all subscriptions from topic map
